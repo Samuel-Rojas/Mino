c3269b46b678723318dbed27edce290ea8976b68
Samuel Rojas <samprogram7@gmail.com>
2025-05-17 20:59:15 -0600

    finished the routing and authentication page, need to finish the sidebar and journal page

--- mino/src/App.tsx
+++ mino/src/App.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Welcome from './pages/Welcome';
 import Login from './pages/Login';
+import Journal from './pages/Journal';
 
 function App() {
     return (
+        <AnimatePresence mode="wait">
         <Routes>
             <Route path="/" element={<Welcome />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/journal" element={<Journal />} />
         </Routes>
+        </AnimatePresence>
     )
 }
 
---
+++ mino/src/pages/Journal.tsx
@@ -0,0 +1,8 @@
+function Journal(){
+    return (
+        <>
+            <h1>Journal Page</h1>
+        </>
+    );
+}
+export default Journal;
