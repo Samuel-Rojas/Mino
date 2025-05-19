a5d0bf9c2974217304171b7454ef6cd49ab98f61
Samuel Rojas <samprogram7@gmail.com>
2025-05-18 23:07:23 -0600

    Finished the frontend mvp, looks horrible but enough to move onto the backend

--- mino/src/App.tsx
+++ mino/src/App.tsx
@@ -3,6 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 import Welcome from './pages/Welcome';
 import Login from './pages/Login';
 import MainPage from './pages/MainPage';
+import SaveJournal from './pages/SaveJournal';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" element={<Welcome />} />
             <Route path="/login" element={<Login />} />
             <Route path="/mainpage" element={<MainPage />} />
+            <Route path="/savejournal" element={<SaveJournal />} />
         </Routes>
         </AnimatePresence>
     )
---
+++ mino/src/pages/SaveJournal.tsx
@@ -0,0 +1,29 @@
+import {motion} from 'framer-motion';
+
+function SaveEntry() {
+
+  const resumeBack = () => {
+    window.location.href = '/mainpage';
+  };
+
+  return (
+    <motion.div
+            initial={{ opacity: 0, x: -70 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 50 }}
+            transition={{ duration: 0.9 }}
+            className="m-0 p-0 flex flex-col items-center justify-center h-screen ">
+            <div>
+                <input type="text" placeholder="Title" className="border-2 border-gray-400 rounded-lg p-2 mb-6 mt-6 w-full" />
+                <input type="date" placeholder="Date" className="border-2 border-gray-400 rounded-lg p-2 mb-6 w-full" />
+            </div>
+            <div className="m-10 p-2">
+                <button onClick={resumeBack} className="rounded-lg before:ease relative h-12 w-40 overflow-hidden border border-emerald-400 bg-emerald-400 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:before:-translate-x-40">
+                    <span className="relative z-10">Submit</span>
+                </button>
+                
+            </div>
+        </motion.div>
+  );
+}
+export default SaveEntry;
